4e7561731960900e2f347edccb3fab0e39fbc09e
joaocottabadaro <joaobadaro@gmail.com>
2018-11-30 02:47:21 -0200

    Add files via upload

--- Pokemon/src/pokemon/trabalhoPokemon.java
+++ Pokemon/src/pokemon/trabalhoPokemon.java
@@ -1,8 +1,43 @@
 package pokemon;
+import classesPokemons.Pikachu;
+import classesPokemons.Blastoise;
+import classesPokemons.Bulbassauro;
+import classesPokemons.Charizard;
+import classesPokemons.Charmander;
+import classesPokemons.Flareon;
+import classesPokemons.Gyrados;
+import classesPokemons.Jolteon;
+import classesPokemons.Meowth;
+import classesPokemons.Squirtle;
+import classesPokemons.Venossauro;
+import classesPokemons.Vaporeon;
+        
+        
+                
 
+        
 class trabalhoPokemon {
 	public static void main(String[] args) {
-
+            Pokemon p1 = new Pikachu();
+            Pokemon p2 = new Blastoise();
+            Pokemon p3 = new Bulbassauro();
+            Pokemon p4 = new Charizard();
+            Pokemon p5 = new Charmander();
+            Pokemon p6 = new Flareon();
+            Pokemon p7 = new Gyrados();
+            Pokemon p8 = new Jolteon();
+            Pokemon p9 = new Meowth();
+            Pokemon p10 = new Venossauro();
+            Pokemon p11 = new Vaporeon();
+            Pokemon p12 = new Squirtle();
+            Time t1 = new Time(p5,p11,p10);
+            Time t2 = new Time(p12,p6,p7);
+            Time t3 = new Time(p3,p8,p2);
+            Time t4 = new Time(p1,p4,p9);
+            
+            
+                   
+                            
 		
 	}
 }

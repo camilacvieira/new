996bb3f28365d7d7c493cb94959d8b4418be2ce8
ArthurDornelas <arthurdornelas99@gmail.com>
2018-11-30 13:36:28 -0200

    att

--- Pokemon/src/pokemon/IniciaJogo.java
+++ Pokemon/src/pokemon/IniciaJogo.java
@@ -23,23 +23,40 @@ import classesPokemons.Vaporeon;
  */
 public class IniciaJogo {
             
+         Pokemon p1;
+         Pokemon p2;
+         Pokemon p3;
+         Pokemon p4;
+         Pokemon p5;
+         Pokemon p6;
+         Pokemon p7;
+         Pokemon p8;
+         Pokemon p9;
+         Pokemon p10;
+         Pokemon p11;
+         Pokemon p12; 
+         Time t1;
+         Time t2;
+         Time t3;
+         Time t4;
+        
         public IniciaJogo(){
-            Pokemon p1 = new Pikachu();
-            Pokemon p2 = new Blastoise();
-            Pokemon p3 = new Bulbassauro();
-            Pokemon p4 = new Charizard();
-            Pokemon p5 = new Charmander();
-            Pokemon p6 = new Flareon();
-            Pokemon p7 = new Gyrados();
-            Pokemon p8 = new Jolteon();
-            Pokemon p9 = new Onix();
-            Pokemon p10 = new Venossauro();
-            Pokemon p11 = new Vaporeon();
-            Pokemon p12 = new Squirtle();
-            Time t1 = new Time(p5,p11,p10);
-            Time t2 = new Time(p12,p6,p7);
-            Time t3 = new Time(p3,p8,p2);
-            Time t4 = new Time(p1,p4,p9);
+            p1 = new Pikachu();
+            p2 = new Blastoise();
+            p3 = new Bulbassauro();
+            p4 = new Charizard();
+            p5 = new Charmander();
+            p6 = new Flareon();
+            p7 = new Gyrados();
+            p8 = new Jolteon();
+            p9 = new Onix();
+            p10 = new Venossauro();
+            p11 = new Vaporeon();
+            p12 = new Squirtle();
+            t1 = new Time(p5,p11,p10);
+            t2 = new Time(p12,p6,p7);
+            t3 = new Time(p3,p8,p2);
+            t4 = new Time(p1,p4,p9);
         }    
             
         public void escolheTime(){ 
@@ -49,10 +66,10 @@ public class IniciaJogo {
             
             // botao escolhe o time escohido1 = t1 ou t2 ou t3 ou t4
            
-            Treinador A = new Treinador(t1);
-            Treinador B = new Treinador(t2);
-            Batalha Bat = new Batalha(A,B);
-                       
+            Treinador a = new Treinador(t1);
+            Treinador b = new Treinador(t2);
+            Batalha bat = new Batalha(a,b);
+            bat.comecarBatalha();           
 		
 	}
 }
